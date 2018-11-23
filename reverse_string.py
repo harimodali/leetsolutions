d01d1e48f76ddd8c6dab3d76db55a508acff7726
Hari Modali <hmodali@Haris-MacBook-Pro-3.local>
2018-11-23 08:46:16 -0800

    344. Reverse String

---
+++ reverse_string.py
@@ -0,0 +1,39 @@
+#Write a function that takes a string as input and returns the string reversed.
+#Input: "hello" Output: "olleh"
+#Input: "A man, a plan, a canal: Panama"
+#Output: "amanaP :lanac a ,nalp a ,nam A"
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        slist = list(s)
+        start = 0
+        end = len(s)-1
+        
+        while start < end:
+            tmp = slist[start]
+            slist[start] = slist[end]
+            slist[end] = tmp
+            start += 1
+            end -=1
+        
+        return ''.join(slist)
+
+    def reverseString2(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        slist = list(s)
+        slist.reverse()
+        return ''.join(slist)
+
+    def reverseString3(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return "".join(reversed(s))
